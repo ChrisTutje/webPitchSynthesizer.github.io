55866d88c35b7e85757d48d7e106398b353f47ad
chris tutje <chris.tutje@gmail.com>
2025-06-18 15:49:20 -0500

    asasd
    dasadsads

---
+++ JS/intervals.js
@@ -0,0 +1,17 @@
+window.intervalPresets = [
+  {
+    name: 'Perfect Fifth (Just)',
+    equation: 'freq * (3/2)',
+    category: 'Just Intonation',
+    textColor: 'black',
+    backgroundColor: 'lightyellow'
+  },
+  {
+    name: 'Perfect Fifth (12-EDO)',
+    equation: 'freq * Math.pow(2, 7/12)',
+    category: '12-EDO',
+    textColor: 'white',
+    backgroundColor: 'darkblue'
+  },
+  // Add more as needed
+];
