098ae482c30530b58907c436b965d04c2a079bc7
chris tutje <chris.tutje@gmail.com>
2025-07-13 14:33:10 -0500

    dadsa
    sdadas

---
+++ JS/chords.js
@@ -0,0 +1,38 @@
+window.chordPresets = [
+  {
+    name: 'Major Triad',
+    symbol: 'M',
+    textColor: 'black',
+    backgroundColor: 'white',
+    rootNote: '1/1',
+    secondVoice: '5/4',
+    thirdVoice: '3/2'
+  },
+  {
+    name: 'Minor Triad',
+    symbol: 'm',
+    textColor: 'white',
+    backgroundColor: 'black',
+    rootNote: '1/1',
+    secondVoice: '6/5',
+    thirdVoice: '3/2'
+  },
+  {
+    name: 'Power Chord',
+    symbol: '5',
+    textColor: 'black',
+    backgroundColor: 'lightgray',
+    rootNote: '1/1',
+    secondVoice: '3/2'
+  },
+  {
+    name: 'Major Seventh',
+    symbol: 'M7',
+    textColor: 'black',
+    backgroundColor: 'white',
+    rootNote: '1/1',
+    secondVoice: '5/4',
+    thirdVoice: '3/2',
+    fourthVoice: '15/8'
+  }
+];
