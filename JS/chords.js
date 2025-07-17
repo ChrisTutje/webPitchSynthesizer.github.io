cc318eb4313a7330238bd06e8ecea5eaec8c1e84
chris tutje <chris.tutje@gmail.com>
2025-07-17 14:01:15 -0500

    sdadsa
    adadssd

--- JS/chords.js
+++ JS/chords.js
@@ -1,38 +1,8 @@
 window.chordPresets = [
-  {
-    name: 'Major Triad',
-    symbol: 'M',
-    textColor: 'black',
-    backgroundColor: 'white',
-    rootNote: '1/1',
-    secondVoice: '5/4',
-    thirdVoice: '3/2'
-  },
-  {
-    name: 'Minor Triad',
-    symbol: 'm',
-    textColor: 'white',
-    backgroundColor: 'black',
-    rootNote: '1/1',
-    secondVoice: '6/5',
-    thirdVoice: '3/2'
-  },
-  {
-    name: 'Power Chord',
-    symbol: '5',
-    textColor: 'black',
-    backgroundColor: 'lightgray',
-    rootNote: '1/1',
-    secondVoice: '3/2'
-  },
-  {
-    name: 'Major Seventh',
-    symbol: 'M7',
-    textColor: 'black',
-    backgroundColor: 'white',
-    rootNote: '1/1',
-    secondVoice: '5/4',
-    thirdVoice: '3/2',
-    fourthVoice: '15/8'
-  }
+  { name: 'Power Chord', symbol: '', textColor: 'black', backgroundColor: 'white', secondVoice: '3/2' },
+
+  { name: 'Major Triad', symbol: 'I', textColor: 'black', backgroundColor: 'white', secondVoice: '5/4', thirdVoice: '3/2' },
+  { name: 'Minor Triad', symbol: 'i', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: '3/2' },
+
+  { name: 'Major Seventh', symbol: 'M7', textColor: 'black', backgroundColor: 'white', secondVoice: '5/4', thirdVoice: '3/2', fourthVoice: '15/8' }
 ];
