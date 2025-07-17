5d1888bd8cc7516320d120edc224bf8e62c24f2c
chris tutje <chris.tutje@gmail.com>
2025-07-17 15:35:47 -0500

    sfdsfd
    sdfsfdd

--- JS/chords.js
+++ JS/chords.js
@@ -11,15 +11,17 @@ window.chordPresets = [
   { name: 'Suspended 4', symbol: 'Sus4', category: 'triadic', textColor: 'black', backgroundColor: 'white', secondVoice: '4/3', thirdVoice: '3/2', fourthVoice: '1/1' },
   { name: 'Lydian Triad', symbol: '', category: 'triadic', textColor: 'black', backgroundColor: 'white', secondVoice: 'Math.pow(2, 6/12)', thirdVoice: '3/2', fourthVoice: '1/1' },
 
-  { name: 'Diminished Seventh', symbol: 'o', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: 'Math.pow(2, 3/12)', thirdVoice: 'Math.pow(2, 6/12)', fourthVoice: 'Math.pow(2, 9/12)' },
-  { name: 'Half-diminished Seventh', symbol: 'o7', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: 'Math.pow(2, 3/12)', fourthVoice: '9/5' },
+  { name: 'Diminished Chord', symbol: 'o', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: 'Math.pow(2, 3/12)', thirdVoice: 'Math.pow(2, 6/12)', fourthVoice: 'Math.pow(2, 9/12)' },
+  { name: 'Half-diminished Seventh', symbol: 'o7', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: 'Math.pow(2, 6/12)', fourthVoice: '9/5' },
   { name: 'Dominant sharp-5', symbol: '', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '5/4', thirdVoice: '8/5', fourthVoice: '9/5' },
   { name: 'Dominant Seventh', symbol: '', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '5/4', thirdVoice: '3/2', fourthVoice: '9/5' },
   { name: 'Minor Seventh', symbol: 'm7', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: '3/2', fourthVoice: '9/5' },
   { name: 'Minor-Major Seventh', symbol: '', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: '3/2', fourthVoice: '15/8' },
   { name: 'Major Seventh', symbol: 'M7', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '5/4', thirdVoice: '3/2', fourthVoice: '15/8' },
 
-  { name: '? Sixth', symbol: '', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: '3/2', fourthVoice: '8/5' },
+  { name: 'Suspended 4/2', symbol: 'Sus4/2', category: 'triadic', textColor: 'black', backgroundColor: 'white', secondVoice: '9/8', thirdVoice: '4/3', fourthVoice: '3/2' },
+
+  { name: '? Sixth', symbol: '', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: '3/2', fourthVoice: '3/2' },
   { name: 'Minor Sixth', symbol: 'm6', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '6/5', thirdVoice: '3/2', fourthVoice: '5/3' },
   { name: '? Sixth', symbol: '', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '5/4', thirdVoice: '3/2', fourthVoice: '8/5' },
   { name: 'Major Sixth', symbol: 'M6', category: 'tetradic', textColor: 'black', backgroundColor: 'white', secondVoice: '5/4', thirdVoice: '3/2', fourthVoice: '5/3' },
